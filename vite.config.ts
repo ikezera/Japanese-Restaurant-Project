86297b819f66119c53af5997295189c5a13a5cbf
ikezera <htmtotal@gmail.com>
2025-03-15 23:01:58 -0300

    changing build files, so i dont get 404 errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Japanese-Restaurant-Project/', // Set base path for GitHub Pages
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    outDir: 'dist', // Output directory for build files
   },
-});
+})
